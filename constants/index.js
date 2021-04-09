edf79a16fc216c54374bafdcf5aca2815815c9a2
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-04-09 14:28:03 +0100

    Initial commit

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export { default as Contributors } from "./contributors.js";
+export { default as Languages } from "./languages.js";
+export { default as logs } from "./logs.js";
+export { default as nouns } from "./nouns.js";
+export { default as singleCharacters } from "./singleCharacters.js";
+export { default as suffixes } from "./suffixes.js";
+export { default as verbs } from "./verbs.js";
---
+++ utils/index.js
@@ -0,0 +1,4 @@
+export { default as Comments } from "./comments.js";
+import * as Helpers from "./helpers.js";
+
+export { Helpers };
